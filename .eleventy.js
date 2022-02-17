0b0cbbe9a5e508d5ce1bed6ee7eb24a092cd349a
Jason Lengstorf <jason@lengstorf.com>
2022-02-17 10:41:40 -0800

    feat: add serverless contrast checker

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,25 @@
+const { EleventyServerlessBundlerPlugin } = require('@11ty/eleventy');
+
+// ewww! (forces the dependency to be included for serverless)
+require('get-contrast');
+
 /**
  * @typedef {import('@11ty/eleventy/src/UserConfig')} EleventyConfig
  * @typedef {ReturnType<import('@11ty/eleventy/src/defaultConfig')>} EleventyReturnValue
  * @type {(eleventyConfig: EleventyConfig)) => EleventyReturnValue}
  */
 module.exports = function (eleventyConfig) {
-	eleventyConfig.addPassthroughCopy('src/css');
-	eleventyConfig.addWatchTarget('src/css');
+  eleventyConfig.addPassthroughCopy('src/css');
+  eleventyConfig.addWatchTarget('src/css');
 
-	return {
-		dir: {
-			input: 'src'
-		}
-	};
+  eleventyConfig.addPlugin(EleventyServerlessBundlerPlugin, {
+    functionsDir: 'netlify/functions',
+    name: 'onrequest',
+  });
+
+  return {
+    dir: {
+      input: 'src',
+    },
+  };
 };
