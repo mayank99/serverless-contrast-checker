96892ceddaabe58905cebeab1f69245e3a521760
Mayank <mayank99@users.noreply.github.com>
2022-02-19 18:14:36 -0500

    Move background and foreground into a variable

--- src/contrast.11tydata.js
+++ src/contrast.11tydata.js
@@ -2,14 +2,23 @@ const contrast = require("get-contrast");
 
 module.exports = {
   eleventyComputed: {
+    foreground: (data) => {
+      if (!data.eleventy.serverless) return '#000000';
+
+      const { foreground } = data.eleventy.serverless.query;
+      if (foreground.startsWith('#') || foreground.startsWith('rgb') || foreground.startsWith('hsl')) return foreground;
+      return `#${foreground}`;
+    },
+    background: (data) => {
+      if (!data.eleventy.serverless) return '#ffffff';
+
+      const { background } = data.eleventy.serverless.query;
+      if (background.startsWith('#') || background.startsWith('rgb') || background.startsWith('hsl')) return background;
+      return `#${background}`;
+    },
     ratio: (data) => {
-      if (!data.eleventy.serverless) {
-        return 1;
-      }
-
-      const { foreground, background } = data.eleventy.serverless.query;
-
-      return contrast.ratio(foreground, background);
+      if (!data.foreground && !data.background) return '1';
+      return contrast.ratio(data.foreground, data.background);
     },
     formattedRatio: (data) => {
       return Number(data.ratio).toFixed(2);
@@ -19,11 +28,9 @@ module.exports = {
         return '';
       }
 
-      const { foreground, background } = data.eleventy.serverless.query;
+      const url = encodeURIComponent(`https://contrast-checker-11ty.netlify.app/contrast/?foreground=${data.foreground.replace('#', '')}&background=${data.background.replace('#', '')}`);
 
-      const url = encodeURIComponent(`https://contrast-checker-11ty.netlify.app/contrast/?foreground=${foreground}&background=${background}`);
-
-      return `https://contrast-checker-11ty.netlify.app/screenshot/${url}/opengraph`;
+      return `https://og-screenshot-11ty.netlify.app/${url}/opengraph`;
     },
   },
 };
