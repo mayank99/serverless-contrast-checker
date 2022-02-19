9f1d368ab662a4542c0dcb10aa7f37118a185e90
Mayank <mayank99@users.noreply.github.com>
2022-02-19 16:49:46 -0500

    Use JS to hardcode url

--- src/contrast.11tydata.js
+++ src/contrast.11tydata.js
@@ -1,4 +1,4 @@
-const contrast = require('get-contrast');
+const contrast = require("get-contrast");
 
 module.exports = {
   eleventyComputed: {
@@ -9,12 +9,21 @@ module.exports = {
 
       const { foreground, background } = data.eleventy.serverless.query;
 
-      console.log({ foreground, background });
-
       return contrast.ratio(foreground, background);
     },
     formattedRatio: (data) => {
       return Number(data.ratio).toFixed(2);
+    },
+    ogImage: (data) => {
+      if (!data.eleventy.serverless) {
+        return null;
+      }
+
+      const { foreground, background } = data.eleventy.serverless.query;
+
+      const url = encodeURIComponent(`https://contrast-checker-11ty.netlify.app/contrast/?foreground=${foreground}&background=${background}`);
+
+      return `https://contrast-checker-11ty.netlify.app/screenshot/${url}/opengraph`;
     },
   },
 };
